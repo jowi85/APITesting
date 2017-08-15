4a2402ef3e1b558817d7d257655ba33e46fc8f62
Joey Miller <miller_joseph3@bah.com>
2017-08-15 16:58:57 -0400

    Added error tests to test.js

--- test.js
+++ test.js
@@ -3,23 +3,45 @@ var should = require('chai').should();
 
 //Basic API Endpoint call and HTTP status code validation
 describe('Sample tests to Blizzard Entertainment\'s public API', function() {
-    it('GET request to endpoint, check HTTP status code is 200', function (done) {
+    it('TC1: Happy path - status code 200', function (done) {
         request.get({url: 'https://us.api.battle.net/wow/character/dalaran/eeyr?fields=stats&locale=en_US&apikey=' + process.env.APIKEY},
             function optionalCallback(err, httpResponse) {
                 httpResponse.statusCode.should.equal(200);
                 done();
             });
     });
-    it('GET request to first endpoint, store value from response, pass value to second endpoint', function (done) {
-        request.get({url: 'https://us.api.battle.net/wow/character/dalaran/eeyr?fields=items&locale=en_US&apikey=' + process.env.APIKEY},
-        function optionalCallback(err, httpResponse) {
-            var itemID = JSON.parse(httpResponse.body).items.chest.id;
-            request.get({url: 'https://us.api.battle.net/wow/item/' + itemID + '?locale=en_US&apikey=' + process.env.APIKEY},
-                function optionalCallback(err, httpResponse) {
-                    httpResponse.statusCode.should.equal(200);
-                    console.log(JSON.parse(httpResponse.body).itemSpells[0].spell.description);
-                    done();
-                });
-        });
+    it('TC2: Invalid API account - status code 403', function (done) {
+        request.get({url: 'https://us.api.battle.net/wow/character/dalaran/eeyr?fields=stats&locale=en_US&apikey='},
+            function optionalCallback(err, httpResponse) {
+                httpResponse.statusCode.should.equal(403);
+                JSON.parse(httpResponse.body).detail.should.equal("Account Inactive");
+                done();
+            });
+    });
+    it('TC3: Invalid character name - status code 404', function (done) {
+        request.get({url: 'https://us.api.battle.net/wow/character/dalaran/notAValidName?fields=stats&locale=en_US&apikey=' + process.env.APIKEY},
+            function optionalCallback(err, httpResponse) {
+                httpResponse.statusCode.should.equal(404);
+                JSON.parse(httpResponse.body).status.should.equal("nok");
+                JSON.parse(httpResponse.body).reason.should.equal("Character not found.");
+                done();
+            });
+    });
+    it('TC4: Invalid realm name - status code 404', function (done) {
+        request.get({url: 'https://us.api.battle.net/wow/character/notAValidName/eeyr?fields=stats&locale=en_US&apikey=' + process.env.APIKEY},
+            function optionalCallback(err, httpResponse) {
+                httpResponse.statusCode.should.equal(404);
+                JSON.parse(httpResponse.body).status.should.equal("nok");
+                JSON.parse(httpResponse.body).reason.should.equal("Realm not found.");
+                done();
+            });
+    });
+    it('TC5: Invalid endpoint - status code 596', function (done) {
+        request.get({url: 'https://us.api.battle.net/thisIsNotAValidEndpoint/wow/character/dalaran/eeyr?fields=stats&locale=en_US&apikey=' + process.env.APIKEY},
+            function optionalCallback(err, httpResponse) {
+                httpResponse.statusCode.should.equal(596);
+                httpResponse.body.should.include("Service Not Found");
+                done();
+            });
     });
 });
